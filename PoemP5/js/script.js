caf4701af7987dce48ef453d72456f76343169f7
Kurji <shaizan.kurji@uleth.ca>
2019-12-02 20:08:54 -0700

    Completed P5

--- PoemP5/js/script.js
+++ PoemP5/js/script.js
@@ -1,9 +1,12 @@
 window.onload = init; 
 
 var video = document.getElementById("backgroundvideo");
+var arrow = document.getElementById("nextArrowId").getElementsByClassName("nextArrow")[0];
 var playing = false; 
 function init(){
     video.onclick = testPlaying;
+    arrow.onclick = transitionImage;
+    document.getElementById("branch").onclick = function() {transitionF()};
 }
 
 function testPlaying(){
@@ -25,10 +28,30 @@ function pauseVideo(){
 
 function playVideo(){
        video.play();
-      for(var i = 1; i <=0; i-=0.2){
-         video.playbackRate = i;
-          setTimeout(function(){}, 1000); 
-    }
  
     playing = true;
+}
+
+function transitionImage(){
+    var part1 = document.getElementById("part1");
+    var p1style = getComputedStyle(part1);
+    
+    
+    var part2 = document.getElementById("part2");
+    var p2style = getComputedStyle(part2);
+    
+    
+    
+  if (p1style.display == "block" && p2style.display == "none") {
+    part1.style.display = "none";
+    part2.style.display = "block";
+  } 
+    else if(p1style.display == "none" && p2style.display == "block") {
+    part2.style.display = "none";
+    part1.style.display = "block";
+}
+}
+
+function transitionF() {
+   window.location.assign("../PoemP6/index.html");
 }
